b4d6307fdc5d4aa23e0ef877bc29ec945db2227d
tommason14 <tom.mason14@gmail.com>
2022-09-05 14:02:45 +0100

    mods

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "UbuntuMono Nerd Font:size=14:antialias=true:autohint=true";
+static char *font = "Hack Nerd Font:size=12:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"emoji:size=14:antialias=true:autohint=true",
@@ -246,6 +246,9 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
+        // with the new sxhkd shortcuts, also have mac-like cmd+c/v
+	{ MODKEY,               XK_C,           clipcopy,       {.i =  0} },
+	{ MODKEY,               XK_V,           clippaste,      {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
